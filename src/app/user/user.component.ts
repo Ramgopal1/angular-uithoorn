5cfd373d40e0152dbe38a7132e5291f73283a1d3
Rajesh <ramgopal.paid@gmail.com>
2022-05-07 11:42:24 +0200

    added user files

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,1 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+  @Input('user')  userName: string;
+ 
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
