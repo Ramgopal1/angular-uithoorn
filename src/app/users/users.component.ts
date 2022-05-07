abbbf813502a11f955fb43ca7e70c263465769eb
Rajesh <ramgopal.paid@gmail.com>
2022-05-07 11:43:02 +0200

    Add files via upload

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,1 +1,22 @@
-
+import { UserComponent } from './../user/user.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { Component, OnInit, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import * as EventEmitter from 'events';
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent implements OnInit {
+  constructor() { }
+  usersList = [];
+  ngOnInit(): void {
+  }
+  onUserAdded (event: string){
+    this.usersList.push(event);
+  }
+  // addUser(){
+  //   this.usersList.push(this.userName);
+  // }
+}
