375467e6911b3856447c0e6708142f1fb0ac6efc
Ramgopal1 <ramgopal.paid@gmail.com>
2022-05-07 20:36:43 +0200

    styling page

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -10,9 +10,13 @@ import { FormsModule } from '@angular/forms';
 export class UsersComponent implements OnInit {
   constructor() {}
   usersList = [];
+  updatedName: string;
   ngOnInit(): void {}
   onUserAdded(event: string) {
     this.usersList.push(event);
+  }
+  updateName() {
+    this.updatedName = 'My updated name is Rajesh';
   }
   // addUser(){
   //   this.usersList.push(this.userName);
