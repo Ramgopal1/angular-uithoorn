95c6997cf7141b6cd47a51513c2fba71dfa54f4f
Rajesh <ramgopal.paid@gmail.com>
2022-05-07 11:35:00 +0200

    Update app.module.ts
    added app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { UsersComponent } from './users/users.component';
+import { UserComponent } from './user/user.component';
+import { AddUserComponent } from './add-user/add-user.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent,
+    UsersComponent,
+    UserComponent,
+    AddUserComponent
+  ],
+  imports: [ BrowserModule, FormsModule, AppRoutingModule ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
