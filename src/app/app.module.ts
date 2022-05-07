b81837f74ae7c3bac62159084ab240a05c92fcda
Ramgopal1 <ramgopal.paid@gmail.com>
2022-05-07 12:22:27 +0200

    Added users module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
+// import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
 import { UserComponent } from './user/user.component';
 import { AddUserComponent } from './add-user/add-user.component';
+import { HelloComponent } from './hello.component';
 
 @NgModule({
   declarations: [
+    HelloComponent,
     AppComponent,
     UsersComponent,
     UserComponent,
-    AddUserComponent
+    AddUserComponent,
   ],
-  imports: [ BrowserModule, FormsModule, AppRoutingModule ],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
